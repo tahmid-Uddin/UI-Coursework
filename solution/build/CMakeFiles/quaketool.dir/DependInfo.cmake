d9b5bf05ac4976872ddadc9e151447a1ee4b482f
Tahmid Uddin [sc23tu] <sc23tu@feng-linux-08.leeds.ac.uk>
2024-11-19 19:27:21 +0000

    cmake working and data set added

---
+++ solution/build/CMakeFiles/quaketool.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/uolstore/home/users/sc23tu/Documents/UI-Coursework/solution/dataset.cpp" "CMakeFiles/quaketool.dir/dataset.cpp.o" "gcc" "CMakeFiles/quaketool.dir/dataset.cpp.o.d"
+  "/uolstore/home/users/sc23tu/Documents/UI-Coursework/solution/main.cpp" "CMakeFiles/quaketool.dir/main.cpp.o" "gcc" "CMakeFiles/quaketool.dir/main.cpp.o.d"
+  "/uolstore/home/users/sc23tu/Documents/UI-Coursework/solution/model.cpp" "CMakeFiles/quaketool.dir/model.cpp.o" "gcc" "CMakeFiles/quaketool.dir/model.cpp.o.d"
+  "/uolstore/home/users/sc23tu/Documents/UI-Coursework/solution/quake.cpp" "CMakeFiles/quaketool.dir/quake.cpp.o" "gcc" "CMakeFiles/quaketool.dir/quake.cpp.o.d"
+  "/uolstore/home/users/sc23tu/Documents/UI-Coursework/solution/build/quaketool_autogen/mocs_compilation.cpp" "CMakeFiles/quaketool.dir/quaketool_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/quaketool.dir/quaketool_autogen/mocs_compilation.cpp.o.d"
+  "/uolstore/home/users/sc23tu/Documents/UI-Coursework/solution/stats.cpp" "CMakeFiles/quaketool.dir/stats.cpp.o" "gcc" "CMakeFiles/quaketool.dir/stats.cpp.o.d"
+  "/uolstore/home/users/sc23tu/Documents/UI-Coursework/solution/window.cpp" "CMakeFiles/quaketool.dir/window.cpp.o" "gcc" "CMakeFiles/quaketool.dir/window.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ solution/build/CMakeFiles/quaketool_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quaketool_autogen"
+  "quaketool_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/quaketool_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
